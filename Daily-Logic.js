0984183acdc6e9cb40d2db4c57cd084c678a4b78
umarshahzad-dev <umarshahzad.seng@gmail.com>
2026-01-22 22:37:18 +0300

    Fix and improved logic in reverse string, longest word, and remove duplicates

--- Daily-Logic.js
+++ Daily-Logic.js
@@ -1,17 +1,17 @@
 // Problem 1: Reverse a string (no .reverse())
 function reverseString(str) {
+    if (!str) return "";
     let reversedString = "";
     for(let i = str.length - 1; i >= 0; i--)
     {
         reversedString += str[i];
     }
-    console.log("Original string: " + str);
-    console.log("Reversed String: " + reversedString);
+    return reversedString;
 }
 
 // Problem 2: Find the longest word in a sentence
-function longestWord(sentence) {
-    const wordArray = sentence.split(" ");
+function findLongestWord(sentence) {
+    const wordArray = sentence.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ").split(" ");
     let longestWord = wordArray[0];
     for(let i = 1; i < wordArray.length; i++)
     {
@@ -20,24 +20,29 @@ function longestWord(sentence) {
             longestWord = wordArray[i];
         }
     }
-    console.log("Longest word in a sentence is: " + longestWord);
+    return longestWord;
 }
 
 // Problem 3: Remove duplicates from an array
 function removeDuplicates(arr) {
-    let newArray = [];
-    for(let i = 0; i < arr.length; i++)
-    {
-        if(!newArray.includes(arr[i]))
-        {
-            newArray.push(arr[i]);
-        }
-    }
-    console.log("Original Array: " + arr);
-    console.log("New Array: " + newArray);
+    const noDuplicate = new Set(arr);
+
+    return [...noDuplicate];
 }
 
 
-reverseString("Reverse");
-longestWord("Finding the longest word in this sentence.");
-removeDuplicates([0, 1, 2, 3, 4, 5, 6, 7, 8, 2, 9]);
+const originalString = "Reverse";
+const reversedString = reverseString(originalString);
+
+console.log("Original String: " , originalString);
+console.log("Reversed String: " , reversedString);
+
+const sentence = "Finding the longest word in this sentence.";
+const longestWord = findLongestWord(sentence);
+console.log("Longest word in a sentence is: " , longestWord);
+
+const originalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 2, 9];
+const noDuplicate = removeDuplicates(originalArray);
+
+console.log("Original Array: " , originalArray);
+console.log("Removed Duplicates: " , noDuplicate);
